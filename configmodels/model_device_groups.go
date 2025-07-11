dc07ae2b6a3746f37f2e9b2ee19a7725021eb882
anaswara <anaswara.n@cdac.in>
2025-07-11 14:31:08 +0530

    updated

--- configmodels/model_device_groups.go
+++ configmodels/model_device_groups.go
@@ -23,9 +23,5 @@ type DeviceGroups struct {
 
 	IpDomainName string `json:"ip-domain-name,omitempty"`
 
-<<<<<<< HEAD
-	IpDomainExpanded []DeviceGroupsIpDomainExpanded `json:"ip-domains,omitempty"`
-=======
 	IpDomainExpanded []DeviceGroupsIpDomainExpanded `json:"ip-domain-expanded,omitempty"`
->>>>>>> temp-merge-branch-anaswara
 }
